6924bfa958e5b9232401e43278c73ee4697f482e
Emerson Silva <emerson_jds@live.com>
2023-09-12 22:33:02 -0300

    changes on structure

--- App.tsx
+++ App.tsx
@@ -1,16 +1,15 @@
 import React, { FC } from 'react';
 import { SafeAreaView, Text, View } from 'react-native';
 
-
 const App: FC = () => {
   return (
-    <>
-      <SafeAreaView className="flex-1">
-        <View className="flex-1 bg-black">
-          <Text className="text-2xl text-white dark:text-white"> / </Text>
-        </View>
-      </SafeAreaView>
-    </>
+
+    <SafeAreaView className="flex-1">
+      <View className="flex-1 bg-black">
+        <Text className="text-2xl text-white dark:text-white"> / </Text>
+      </View>
+    </SafeAreaView>
+
   )
 }
 
