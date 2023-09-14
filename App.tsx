1367e48fa38eb9b9f8dfdd35d333dc4a3d8324dc
Emerson Silva <emerson_jds@live.com>
2023-09-13 23:43:36 -0300

    adjust on scope

--- App.tsx
+++ App.tsx
@@ -3,13 +3,13 @@ import { SafeAreaView, Text, View } from 'react-native';
 
 const App: FC = () => {
   return (
-
-    <SafeAreaView className="flex-1">
-      <View className="flex-1 bg-black">
-        <Text className="text-2xl text-white dark:text-white"> / </Text>
-      </View>
-    </SafeAreaView>
-
+    <>
+      <SafeAreaView className="flex-1">
+        <View className="flex-1 bg-black">
+          <Text className="text-2xl text-white dark:text-white"> / </Text>
+        </View>
+      </SafeAreaView>
+    </>
   )
 }
 
