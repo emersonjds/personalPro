6d277045c18eded9023774b767df19bf0e3d529f
Emerson Silva <emerson_jds@live.com>
2023-09-14 23:33:09 -0300

    create store

---
+++ src/store/index.tsx
@@ -0,0 +1,22 @@
+import { createSlice, configureStore } from '@reduxjs/toolkit'
+
+
+const initialState = {
+  count: 0
+}
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.count++
+    }
+  }
+})
+
+const store = configureStore({
+  reducer: counterSlice.reducer
+})
+
+export const { increment } = counterSlice.actions
