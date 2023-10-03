95535d539cc4123266b6f5a9060c1cb1676e2076
Emerson Silva <emerson_jds@live.com>
2023-10-02 23:50:40 -0300

    add changes

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,7 +3,13 @@ import { Text, View } from 'react-native';
 
 const Login: React.FC = () => {
   return (
-    <></>
+    <>
+      <View>
+        <Text>
+          Changes
+        </Text>
+      </View>
+    </>
   )
 }
 
