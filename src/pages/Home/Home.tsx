e710b2f6208992f28a7f43bd92578801152a3f6f
Emerson Silva <emerson_jds@live.com>
2023-11-06 21:45:05 -0300

    tests for branch

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,7 +6,7 @@ const Home: React.FC = () => {
     <>
       <View className='flex-1 justify-center items-center bg-gray-200'>
         <Text>
-          Personal
+          Home of Project
         </Text>
       </View>
     </>
