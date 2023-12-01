df23ab7214b95d60ae26a82a3d904cfe8d78479f
Emerson Silva <emerson_jds@live.com>
2023-12-01 19:19:26 -0300

    eslint

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,16 +1,14 @@
-import React, { FC } from 'react';
-import { Text, View } from 'react-native';
+import React, {FC} from 'react';
+import {Text, View} from 'react-native';
 
 const Login: FC = () => {
   return (
     <>
       <View>
-        <Text>
-          Changes
-        </Text>
+        <Text>Changes</Text>
       </View>
     </>
-  )
-}
+  );
+};
 
 export default Login;
