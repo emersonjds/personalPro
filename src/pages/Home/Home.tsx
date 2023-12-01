2d212561034a6d3d3d8bea1b51ce83e46e625cca
Emerson Silva <emerson_jds@live.com>
2023-12-01 19:19:02 -0300

    adjust file with eslint

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {Text, View} from 'react-native';
 
 const Home: React.FC = () => {
   return (
     <>
-      <View className='flex-1 justify-center items-center bg-gray-200'>
-        <Text className='text-2xl'>Home</Text>
+      <View className="flex-1 justify-center items-center bg-gray-200">
+        <Text className="text-2xl">Home</Text>
       </View>
     </>
-  )
-}
+  );
+};
 
 export default Home;
