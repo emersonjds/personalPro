081a9dd7bdbcdb3f2c5365a0a156f3d900d15fef
Emerson Silva <emerson_jds@live.com>
2023-12-04 22:09:37 -0300

    adjust error page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import { Text, View } from 'react-native';
 
 const Home: React.FC = () => {
   return (
     <>
       <View className="flex-1 justify-center items-center bg-gray-200">
-        <Text className="text-2xl">Home</Text>
+        <Text className="text-2xl"></Text>
       </View>
     </>
   );
